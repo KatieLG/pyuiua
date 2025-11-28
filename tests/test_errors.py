1cba7e31f9f07ba2b3bdf3d8c533f68e12ae426a
Katie <5213160+KatieLG@users.noreply.github.com>
2025-11-28 00:02:56 +0000

    tests: remove some redundancy from tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,13 +8,12 @@ def test_pop_empty_stack_error(uiua: Uiua) -> None:
         uiua.pop()
 
 
-@pytest.mark.parametrize("code", ["invalid syntax", "+ 1"])
-def test_invalid_uiua_code(uiua: Uiua, code: str) -> None:
+def test_invalid_uiua_code(uiua: Uiua) -> None:
     with pytest.raises(UiuaError):
-        uiua.run(code)
+        uiua.run("invalid syntax")
 
 
 @pytest.mark.parametrize("unsupported_type", [{"key": "value"}, object(), {1, 2, 3}])
-def test_invalid_python_type(uiua: Uiua, unsupported_type: dict | object | set) -> None:
+def test_unsupported_python_type(uiua: Uiua, unsupported_type: dict | object | set) -> None:
     with pytest.raises(TypeError):
         uiua.push(unsupported_type)  # type: ignore
